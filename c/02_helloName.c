87f90efb0d2b02a3724117845072ac291ba91a69
Mohamed Aamir <paulholm123456@gmail.com>
2026-05-05 00:21:35 +0530

    c02 added

---
+++ c/02_helloName.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// write after the placeholder to include more text - don't open another set of text after the variable
+
+int main(){
+    char name[]="Aamir";
+    printf("Hello %s!\n", name); // %s is the placeholder for char
+    return 0;
+}

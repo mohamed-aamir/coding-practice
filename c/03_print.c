769ebf169f1832d8e28565afdb58e85ddc7835b3
Mohamed Aamir <paulholm123456@gmail.com>
2026-05-05 00:28:56 +0530

    c03 added

---
+++ c/03_print.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// This is a sinlge line comment
+
+/* And this is a 
+multi line comment */
+
+int main(){
+    printf("This will take you to the next line. a\nb\n");
+    printf("This will include a backslash. a\\b\n");
+    printf("This will include a horizontal tab. a\tb\n");
+    printf("This will include a double quote. a\"b\n");
+    return 0;
+}

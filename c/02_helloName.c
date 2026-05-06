37efdf6bb0f44e8b3d4349ed3b6ed64aabf8c5eb
Mohamed Aamir <mohamedaamir08@gmail.com>
2026-05-06 18:13:43 +0530

    old files deleted

--- c/02_helloName.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-// write after the placeholder to include more text - don't open another set of text after the variable
-
-int main(){
-    char name[]="Aamir";
-    printf("Hello %s!\n", name); // %s is the placeholder for char
-    return 0;
-}

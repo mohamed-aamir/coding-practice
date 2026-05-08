78633c5f274c876f1afdd4d693b2bffb5b52171d
Mohamed Aamir <mohamedaamir08@gmail.com>
2026-05-08 14:09:24 +0530

    c07 added

---
+++ c/c07.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    char ch;
+    printf("Enter the character to find its ASCII value: ");
+    scanf("%c", &ch);
+    printf("The ASCII value of %c is %d\n", ch, ch);
+    return 0;
+}
